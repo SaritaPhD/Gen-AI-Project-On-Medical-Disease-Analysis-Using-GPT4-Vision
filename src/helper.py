517ffe78f90e011a1842d0be235cb1ff47264737
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2025-01-13 17:53:56 +0530

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -3,5 +3,5 @@ You will be provided with images and you need to identify the anomalies, any dis
 Write all the findings, next steps, recommendation, etc. You only need to respond if the image is related to a human body and health issues. 
 You must have to answer but also write a disclaimer saying that "Consult with a Doctor before making any decisions".
 Remember, if certain aspects are not clear from the image, it's okay to state 'Unable to determine based on the provided image.'
-
+ 
 Now analyze the image and answer the above questions in the same structured manner defined above."""
